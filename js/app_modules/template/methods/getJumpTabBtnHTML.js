8e496ea86536bca2c466cf4a4672f019e210df88
mirko77 <mirko.menegazzo@gmail.com>
2024-08-02 18:40:34 +0200

    [TASK-3523] Improved Jumps semantic (added IF - ELSE label)

--- js/app_modules/template/methods/getJumpTabBtnHTML.js
+++ js/app_modules/template/methods/getJumpTabBtnHTML.js
@@ -5,7 +5,6 @@ var getJumpTabBtnHTML = function (ref, is_active) {
     var html = '';
 
     if (is_active) {
-
         html += '<li role="presentation" class="jump-tab nav-tabs__tab-btn-item">';
         html += '<a href="#jumps-' + ref + '" role="tab" data-toggle="tab" > Jumps (IF - ELSE)&nbsp;';
         html += '<i class="fa fa-exclamation fa-2x invisible jumps-error"></i>';
@@ -13,7 +12,6 @@ var getJumpTabBtnHTML = function (ref, is_active) {
         html += '</li>';
     }
     else {
-
         html += '<li role="presentation" class="jump-tab disabled disabled-tab nav-tabs__tab-btn-item">';
         html += '<a href="#" class="nav-tabs__jump" role="tab" > Jumps (IF - ELSE)&nbsp;';
         html += '<i class="fa fa-exclamation fa-2x invisible jumps-error"></i>';
